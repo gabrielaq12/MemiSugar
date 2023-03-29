3f910652655304fb4291d4f523f8eb259fe8d98d
gabriela quevedo <gabrielaquevedo12@hotmail.com>
2023-03-29 11:19:55 -0300

    carrito completado

--- section/script.js
+++ section/script.js
@@ -7,58 +7,76 @@ class Venta {
     }
 }
 
-function setUp(){
-    localStorage.setItem("carrito", JSON.stringify([]));
-    const quesillo = new Producto ("quesillo", 500);
-    const pie = new Producto ("pie", 550);
-    const pinata = new Producto ("pinata", 480 );
-    console.log("setUp listo");
-    console.log(quesillo);
-}
-
 function agregarAlCarrito(producto){
     let carrito = JSON.parse(localStorage.getItem("carrito"));
 
     let existe = carrito.findIndex(function(item){
-        return item.producto == producto;
+        return item.producto.sabor == producto.sabor;
     });
     if(existe == -1){
         let venta = new Venta (producto, producto.precio );
+        venta.cantidad = 1;
         carrito.push(venta);
     } else {
         carrito[existe].cantidad += 1;
-        carrito[existe].total += cantidad * producto.precio;
+        carrito[existe].total += carrito[existe].cantidad * producto.precio;
     }
     localStorage.setItem("carrito", JSON.stringify(carrito));
-    console.log(carrito);
 }
 
 function vaciarCarrito(){
     localStorage.setItem("carrito", JSON.stringify([]));
-
+    refrescarCarrito();
 }
 
 function calcularTotalCarrito(){
     let total = 0;
-    let carrito = JSON.parse(localStorage.getItem(carrito));
+    let carrito = JSON.parse(localStorage.getItem("carrito"));
     carrito.forEach(function(element){
         total += element.total; 
     });
     return total;
 }
 
+function refrescarCarrito(){
+    let cart = document.getElementById("cart");
+    cart.style.display = "block";
+    var itemsContainer = document.getElementById("products");
+    itemsContainer.innerHTML = "";
+    let totalContainer = document.getElementById("total");
+    totalContainer.innerHTML = "";
+    let carrito = JSON.parse(localStorage.getItem("carrito"));
+    carrito.forEach(function(element){
+        let elementDiv = document.createElement("div");
+        elementDiv.innerHTML = element.producto.sabor + " x " + element.cantidad + " = " + element.total;
+        itemsContainer.appendChild(elementDiv);
+    });
+    let elementDiv = document.createElement("div");
+    elementDiv.innerHTML = "$"+calcularTotalCarrito();
+    totalContainer.appendChild(elementDiv);
+}
+
 let btnClickServicioQuesillo = document.getElementById("btnClickServicioQuesillo");
-btnClickServicioQuesillo.addEventListener("click", function(){agregarAlCarrito(quesillo)});
+btnClickServicioQuesillo.addEventListener("click", function(){
+    agregarAlCarrito(quesillo);
+    refrescarCarrito();
+});
 
 let btnClickServicioPinata = document.getElementById("btnClickServicioPinata");
-btnClickServicioPinata.addEventListener("click", function(){agregarAlCarrito(pinata)});
+btnClickServicioPinata.addEventListener("click", function()
+{   
+    agregarAlCarrito(pinata);
+    refrescarCarrito();
+});
 
 let btnClickServicioPie = document.getElementById("btnClickServicioPie");
-btnClickServicioPie.addEventListener("click", function(){agregarAlCarrito(pie)});
+btnClickServicioPie.addEventListener("click", function(){
+    agregarAlCarrito(pie);
+    refrescarCarrito();
+});
 
 let btnVaciarCarrito = document.getElementById("btnVaciarCarrito");
 btnVaciarCarrito.addEventListener("click", vaciarCarrito);
-
 
 
 
